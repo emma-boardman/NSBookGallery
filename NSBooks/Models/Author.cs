71c8d5b399c63a608d410514cf0a1e027b091811
Emma Boardman <emma@emmaboardman.co.uk>
2018-12-31 15:38:46 +0000

    Added Book & Author models

---
+++ NSBooks/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace NSBooks.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ NSBooks/Models/NSBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NSBooks.Models
+{
+    public class NSBook
+    {       
+            public int Id { get; set; }
+            public string SeriesTitle { get; set; }
+            public int IssueNumber { get; set; }
+            public string DescriptionHtml { get; set; }
+            public Author[] Authors { get; set; }
+            public bool Favourite { get; set; }
+
+    }
+}
