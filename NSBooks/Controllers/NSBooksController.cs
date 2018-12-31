fd138723af7fd45a1fd7120ede533218a8edc74c
Emma Boardman <emma@emmaboardman.co.uk>
2018-12-31 13:41:58 +0000

    Updated Detail action method to use Action type

--- NSBooks/Controllers/NSBooksController.cs
+++ NSBooks/Controllers/NSBooksController.cs
@@ -5,8 +5,17 @@ namespace NSBooks.Controllers
 {
     public class NSBooksController : Controller
     {
-        public NSBooksController()
+     
+        public ActionResult Detail()
+
         {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from NS Books Controller");
+           
         }
     }
 }
