961184c2745c7821785dbffae441eba03a7babd6
Emma Boardman <emma@emmaboardman.co.uk>
2018-12-31 13:52:39 +0000

    Added Detail View

--- NSBooks/Controllers/NSBooksController.cs
+++ NSBooks/Controllers/NSBooksController.cs
@@ -9,13 +9,7 @@ namespace NSBooks.Controllers
         public ActionResult Detail()
 
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from NS Books Controller");
-           
+            return View();
         }
     }
 }
