ca5b53e72e32b048570130df6c6f225456031add
Emma Boardman <emma@emmaboardman.co.uk>
2018-12-31 14:58:43 +0000

    Updated NSBook View to use ViewBag

--- NSBooks/Controllers/NSBooksController.cs
+++ NSBooks/Controllers/NSBooksController.cs
@@ -9,6 +9,20 @@ namespace NSBooks.Controllers
         public ActionResult Detail()
 
         {
+
+            ViewBag.SeriesTitle = "Kanji Practice";
+            ViewBag.JlptLevel = 4;
+            ViewBag.Description = "<b>Info Info Info</b>";
+            ViewBag.Authors = new string[]
+
+                {
+                    "Script: Test this isn't a cached value",
+                    "Pencils: Humberto Ramos",
+                    "Inks: Victor Olazaba",
+                    "Colors: Edgar Delgado",
+                    "Letters: Chris Eliopoulos"
+                };
+
             return View();
         }
     }
